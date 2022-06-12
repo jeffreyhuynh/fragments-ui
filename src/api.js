91aeb9cdbcc1ad5078901fcb4c21e5be863f3d4e
JH <>
2022-06-12 00:31:16 -0400

    dev environment setup

--- src/api.js
+++ src/api.js
@@ -3,10 +3,10 @@
 const apiUrl = process.env.API_URL || 'http://localhost:8080';
 
 export async function getUserFragments(user) {
-    console.log('requesting user fragments data...');
-    try {
-      const res = await fetch(`${apiUrl}/v1/fragments`, {
-        headers: user.authorizationHeaders(),
+  console.log('requesting user fragments data...');
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      headers: user.authorizationHeaders(),
     });
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
--- src/app.js
+++ src/app.js
@@ -4,30 +4,30 @@ import { getUserFragments } from './api';
 import { Auth, getUser } from './auth';
 
 async function init() {
-    const userSection = document.querySelector('#user');
-    const loginBtn = document.querySelector('#login');
-    const logoutBtn = document.querySelector('#logout');
+  const userSection = document.querySelector('#user');
+  const loginBtn = document.querySelector('#login');
+  const logoutBtn = document.querySelector('#logout');
 
-    loginBtn.onclick = () => {
-        Auth.federatedSignIn();
-    };
+  loginBtn.onclick = () => {
+    Auth.federatedSignIn();
+  };
 
-    logoutBtn.onclick = () => {
-        Auth.signOut();
-    };
+  logoutBtn.onclick = () => {
+    Auth.signOut();
+  };
 
-    const user = await getUser();
-    if (!user) {
-        logoutBtn.disabled = true;
-        return;
-    }
+  const user = await getUser();
+  if (!user) {
+    logoutBtn.disabled = true;
+    return;
+  }
 
-    await getUserFragments(user);
+  await getUserFragments(user);
 
-    console.log ({ user });
-    userSection.hidden = false;
-    userSection.querySelector('.username').innerText = user.username;
-    loginBtn.disabled = true;
+  console.log({ user });
+  userSection.hidden = false;
+  userSection.querySelector('.username').innerText = user.username;
+  loginBtn.disabled = true;
 }
 
 addEventListener('DOMContentLoaded', init);
