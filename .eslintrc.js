647ddaebc72eb5d11aef3fd1b57323507ef90ff7
JH <>
2022-06-14 08:10:58 -0400

    added /GET/:ID, /POST requesters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import { getUserFragments } from './api';
 import { Auth, getUser } from './auth';
 
 async function init() {
+  const apiUrl = process.env.API_URL || 'http://localhost:8080';
   const userSection = document.querySelector('#user');
+  const welcomeSection = document.querySelector('#welcome');
   const loginBtn = document.querySelector('#login');
   const logoutBtn = document.querySelector('#logout');
 
@@ -21,13 +23,61 @@ async function init() {
     logoutBtn.disabled = true;
     return;
   }
-
   await getUserFragments(user);
 
   console.log({ user });
   userSection.hidden = false;
-  userSection.querySelector('.username').innerText = user.username;
+  welcomeSection.hidden = false;
+  welcomeSection.querySelector('.username').innerText = user.username;
   loginBtn.disabled = true;
+
+  // consider adding error handling to support 415/invalid type
+  document.getElementById('post').addEventListener('click', () => {
+    const data = document.getElementById('postText').value;
+    const url = apiUrl + '/v1/fragments/';
+    fetch(url, {
+      method: 'POST',
+      headers: user.authorizationHeaders('text/plain'),
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('POST ' + url + ': response: ' + JSON.stringify(data, null, 4));
+        document.getElementById('postResponse').innerText = JSON.stringify(data, null, 4);
+        document.getElementById('responseLink').value = url + data.fragment.id;
+      });
+  });
+
+  // consider ensuring that this field cannot be empty (it returns a fragments list otherwise)
+  document.getElementById('get').addEventListener('click', () => {
+    let url = document.getElementById('getText').value;
+    if (!RegExp(/(https?:\/\/)/).test(url)) {
+      // matches start of a link
+      url = apiUrl + '/v1/fragments/' + url; // received id, reconstruct into link for http request
+    }
+    fetch(url, {
+      method: 'GET',
+      headers: user.authorizationHeaders(),
+    })
+      .then((res) => res.text())
+      .then((data) => {
+        console.log('GET ' + url + ': response: ' + data);
+        document.getElementById('getResponse').value = data;
+      });
+  });
+
+  document.getElementById('getLink').addEventListener('click', () => {
+    let url = document.getElementById('responseLink').value;
+    fetch(url, {
+      method: 'GET',
+      headers: user.authorizationHeaders(),
+    })
+      .then((res) => res.text())
+      .then((data) => {
+        console.log('GET ' + url + ': response: ' + data);
+        document.getElementById('getResponse').value = data;
+      });
+  });
 }
 
 addEventListener('DOMContentLoaded', init);
